2a35ed92d22aed1187dd16aa21348c851caedd5f
s813082 <s813082@gmail.com>
2018-05-04 15:47:44 +0800

    Timer final

--- src/main/java/Metrics_03_Timer.java
+++ src/main/java/Metrics_03_Timer.java
@@ -2,6 +2,7 @@ import com.codahale.metrics.ConsoleReporter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.Timer;
 
+import java.util.Random;
 import java.util.concurrent.TimeUnit;
 
 public class Metrics_03_Timer {
@@ -25,6 +26,8 @@ public class Metrics_03_Timer {
         //讓console Report 每五秒打印一次統計結果
         reporter.start(5, TimeUnit.SECONDS);
 
+        Random random = new Random();
+
         // *** 執行業務邏輯的coding block <<START>> *** //
         System.out.println("===== Coding Block <<S>> ======");
         long start = System.nanoTime(); // 整個coding blocking的計時開始
@@ -40,6 +43,7 @@ public class Metrics_03_Timer {
 
             // => do something --- start
             COUNTER_CODING_BLOCK++;
+            Thread.sleep(random.nextInt(100));
             // => do something --- end
 
             context.close();//計時結束
@@ -59,6 +63,28 @@ public class Metrics_03_Timer {
         System.out.println("Total operations  : " + operations_count + " ops");
         System.out.println("Total time spent  : " + (end-start) + " nanos || " + (end-start)/(float)1000000 + " millis || " + (end-start)/(float)1000000000 +"seconds");
         System.out.println("Average ratio     : " + (((end-start) / (float)operations_count)) * 1000000 + " ops/millis sec");
+
+        /*
+        *              count = 169
+         mean rate = 16.13 calls/second   每秒平均
+         1-minute rate = 14.47 calls/second 最近一分鐘平均
+         5-minute rate = 14.26 calls/second
+         15-minute rate = 14.22 calls/second
+               min = 1.91 milliseconds 最小直
+               max = 173.95 milliseconds 最大直
+              mean = 58.46 milliseconds 平均
+            stddev = 32.65 milliseconds 標準差
+            median = 55.44 milliseconds 中位數
+              75% <= 85.79 milliseconds
+              95% <= 109.82 milliseconds
+              98% <= 123.31 milliseconds
+              99% <= 135.01 milliseconds
+            99.9% <= 173.95 milliseconds
+        *
+        *
+        *
+        *
+        * */
 
 
     }
