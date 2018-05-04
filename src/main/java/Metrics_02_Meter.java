731fd7b0a91bc3c57859b5b7bc955a99b5ad26a0
s813082 <s813082@gmail.com>
2018-05-04 15:20:20 +0800

    Add 2

--- src/main/java/Metrics_02_Meter.java
+++ src/main/java/Metrics_02_Meter.java
@@ -1,6 +1,29 @@
+import com.codahale.metrics.ConsoleReporter;
+import com.codahale.metrics.Meter;
+import com.codahale.metrics.MetricRegistry;
+
+import java.util.concurrent.TimeUnit;
+
 public class Metrics_02_Meter {
 
-    public static void main(String[] args) {
+
+
+    //產生一個MetricRegistry 的 instance
+    private static final MetricRegistry metrics = new MetricRegistry();
+
+    //註冊一個meter的metric
+    private static  final Meter ops_meter = metrics.meter("com.wistron.witlab.ops_meter");
+
+    public static void main(String[] args) throws InterruptedException {
+
+        //產生一個consolereporter的instance來定的
+        ConsoleReporter reporter = ConsoleReporter.forRegistry(metrics)
+                .convertDurationsTo(TimeUnit.SECONDS)
+                .convertDurationsTo(TimeUnit.MILLISECONDS)
+                .build();
+
+        //讓console Report 每五秒打印一次統計結果
+        reporter.start(5, TimeUnit.SECONDS);
 
         // *** 執行業務邏輯的coding block <<START>> *** //
         System.out.println("===== Coding Block <<S>> ======");
@@ -14,9 +37,13 @@ public class Metrics_02_Meter {
         for(int i=0; i< operations_count; i++){
             // => do something --- start
             COUNTER_CODING_BLOCK++;
+            Thread.sleep( 10);
             // => do something --- end
+
+            ops_meter.mark(); //紀錄備執行一次
         }
 
+        //1 minute rate 離打印時間最近的一分鐘的執行速度
 
         long end = System.nanoTime(); // 整個coding blocking的計時開始
         System.out.println("===== Coding Block <<E>> ======");
@@ -30,6 +57,7 @@ public class Metrics_02_Meter {
         System.out.println("Total operations  : " + operations_count + " ops");
         System.out.println("Total time spent  : " + (end-start) + " nanos || " + (end-start)/(float)1000000 + " millis || " + (end-start)/(float)1000000000 +"seconds");
         System.out.println("Average ratio     : " + (((end-start) / (float)operations_count)) * 1000000 + " ops/millis sec");
+
 
     }
 }
